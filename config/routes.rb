96f1342c130ad832f3c39014f4e4def1f4f67a6c
forejy.joffrey <joffrey.marques@gmail.com>
2020-02-18 14:11:23 +0100

    Changement url action mailer pour heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
+  # get 'static_pages/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "static_pages#index"
+  root to: "static_pages#index" 
 end
